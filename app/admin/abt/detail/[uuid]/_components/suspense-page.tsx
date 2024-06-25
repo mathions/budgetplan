5b4048fe2c7d53203de86e543dca95cd8e1619a0
duta <222011704@stis.ac.id>
2024-06-25 22:47:44 +0700

    detail abt

--- app/admin/abt/detail/[uuid]/_components/suspense-page.tsx
+++ app/admin/abt/detail/[uuid]/_components/suspense-page.tsx
@@ -7,20 +7,42 @@ export default async function SuspensePage({ uuid, token, }: { uuid: string; tok
   const detail = await getDetailAbt(token, uuid)
 
   return (
-    <Card className="p-8 space-y-6">
-      <div className="space-y-3">
-        <div className="flex justify-between">
-          <div className="font-semibold text-xl text-textweak">
-            {detail?.office}
+    <div className="space-y-12">
+      <div className="border-t border-border h-[1px]"></div>
+      <div className="space-y-6">
+        <h4>Detail</h4>
+        <Card className="p-8 space-y-2">
+          <div className="space-y-1 mb-3">
+            <p className="text-textweak">Perihal</p>
+            <h5 className="font-semibold">{detail?.perihal}</h5>
           </div>
-          <div className="w-fit">
-            <StatusAbt statuss={detail?.status} />
+          <div className="grid grid-cols-3">
+            <div className="col-span-1 space-y-1">
+              <p className="text-textweak">Satuan Kerja</p>
+              <p className="font-semibold">{detail?.office}</p>
+            </div>
+            <div className="col-span-1 space-y-1">
+              <p className="text-textweak">Tanggal Pengajuan</p>
+              <p className="font-semibold">{detail?.created_at}</p>
+            </div>
+            <div className="col-span-1 space-y-1">
+              <p className="text-textweak">Status</p>
+              <div className="flex"><StatusAbt statuss={detail?.status}/></div>
+            </div>
           </div>
-        </div>
-        <h4>{detail?.perihal}</h4>
-        <div className="text-textweak">{detail?.created_at}</div>
+          <div className="space-y-1">
+            <p className="text-textweak">Catatan</p>
+            <p className="font-semibold">{detail?.note}</p>
+          </div>
+        </Card>
       </div>
-      <PdfViewer uuid={uuid} token={token} />
-    </Card>
+      <div className="border-t border-border h-[1px]"></div>
+      <div className="space-y-6">
+        <h4>Dokumen Brafaks</h4>
+        <Card className="p-8">
+          <PdfViewer uuid={uuid} token={token} />
+        </Card>
+      </div>
+    </div>
   );
 }
